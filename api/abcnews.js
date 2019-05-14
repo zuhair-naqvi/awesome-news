e6e1d0470f3d7c83fd1e0a1f4e453ee925ab856a
doc-manhattan <d.mhtn.init@gmail.com>
2019-05-15 05:56:13 +1000

    newsfeed fetched

---
+++ api/abcnews.js
@@ -0,0 +1,13 @@
+const getNews = async () => {
+  try {
+    let response = await fetch(
+      "https://api.rss2json.com/v1/api.json?rss_url=http://www.abc.net.au/news/feed/51120/rss.xml"
+    );
+    let responseJson = await response.json();
+    return responseJson;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { getNews };
